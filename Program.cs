840194da1ad6a9e8b645a866356c9e973c43a582
facundotobio <facundohectortobio@gmail.com>
2023-06-01 12:05:20 -0300

    First ejercise

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- 
+Console.WriteLine("Escriba su nombre");
+string nombre = Console.ReadLine();;
+Console.WriteLine(nombre);
+string hora = DateTime.Now.ToString("h:mm:ss tt");
+Console.WriteLine("La hora es {0}", hora);
